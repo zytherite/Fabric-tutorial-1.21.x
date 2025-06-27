0e87ee7399d1e90fb5b7463735653c2619a54ffd
zytherite <zytherite971@gmail.com>
2025-06-27 18:47:39 +0400

    Added 2 items: Greeney Bluey and Raw Pink Garnet

--- src/main/java/net/faiz/tutorialmod/TutorialMod.java
+++ src/main/java/net/faiz/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.faiz.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.faiz.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //Very important comment
@@ -11,6 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/faiz/tutorialmod/TutorialModClient.java
+++ src/main/java/net/faiz/tutorialmod/TutorialModClient.java
@@ -1,10 +1,11 @@
 package net.faiz.tutorialmod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.faiz.tutorialmod.item.ModItems;
 
 public class TutorialModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        //ModItems.registerModItems();
     }
 }
