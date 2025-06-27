cbc186f562a6db25ca278b5514793b16be769115
zytherite <zytherite971@gmail.com>
2025-06-27 16:45:17 +0400

    Added a very important comment

--- src/main/java/net/faiz/tutorialmod/TutorialMod.java
+++ src/main/java/net/faiz/tutorialmod/TutorialMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//Very important comment
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorial-mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
